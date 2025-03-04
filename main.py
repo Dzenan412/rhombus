1557816faeb6f6eaf41be83b47c8a6aaeab6c503
28zuzicd <28zuzicd@bwschools.net>
2025-03-04 09:44:11 -0500

    commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import turtle
+
+screen = turtle.Screen()
+screen.screensize( 500, 500)
+screen.bgcolor('yellow')
+
+t = turtle.Turtle()
+# t.write("Hello",font=("Arial",30,"bold italic"),align="center")
+
+t.speed(0)
+t.setheading(45)
+t.fillcolor('brown')
+t.begin_fill()
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.end_fill()
+
+t.speed(0)
+t.setheading(135)
+t.fillcolor('cyan')
+t.begin_fill()
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.end_fill()
+
+t.hideturtle()
+turtle.done()
